bafc107746fcc51d3578091b5a4377a6899118af
Erick <erickaleruano@gmail.com>
2021-10-20 17:01:39 -0600

    Export available modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import initModule from './modules/init';
 import buildModule from './modules/build';
 
+export const init = initModule;
 export const build = buildModule;
 
 export default {
+  init,
   build,
 };
