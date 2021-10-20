d581ee3956418b4bd928572e02fb908a8fa75648
Erick <erickaleruano@gmail.com>
2021-10-20 16:57:43 -0600

    Update build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  plugins: [resolve()],
+  external: ['fs-extra', 'decompress'],
+  plugins: [json(), resolve()],
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', exports: 'named' },
+    { file: pkg.module, format: 'es', exports: 'named' },
   ],
 };
