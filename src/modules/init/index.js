39cd96c0dbccd21971ed0ed46235f519cde90e12
Erick <erickaleruano@gmail.com>
2021-10-20 17:57:37 -0600

    Attempt to download template if not in map

--- src/modules/init/index.js
+++ src/modules/init/index.js
@@ -6,18 +6,23 @@ import unzipTemplate from './unzipTemplate';
 import removeTemplate from './removeTemplate';
 
 const init = async (template = 'init') => {
-  const tmpPath = path.join(process.cwd(), 'tmp');
-  const downloadedTemplatePath = await downloadTemplate(
-    templates[template],
-    tmpPath,
-  );
-  const [{ path: unzippedTemplatePath }] = await unzipTemplate(
-    downloadedTemplatePath,
-    tmpPath,
-  );
-  await copyTemplate(path.join(tmpPath, unzippedTemplatePath), process.cwd());
-  await removeTemplate(tmpPath);
-  return true;
+  try {
+    const tmpPath = path.join(process.cwd(), 'tmp');
+    const downloadedTemplatePath = await downloadTemplate(
+      template in templates ? templates[template] : template,
+      tmpPath,
+    );
+    const [{ path: unzippedTemplatePath }] = await unzipTemplate(
+      downloadedTemplatePath,
+      tmpPath,
+    );
+    await copyTemplate(path.join(tmpPath, unzippedTemplatePath), process.cwd());
+    await removeTemplate(tmpPath);
+    return true;
+  } catch (err) {
+    console.log('Invalid template or URL provided');
+    return false;
+  }
 };
 
 export default init;
